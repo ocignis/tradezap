8ef38f465c060d84b97948d32a829dde5df02b07
mkosir <kosir.mar@gmail.com>
2023-09-14 22:42:16 +0200

    chore(deps): update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import { cac } from 'cac';
+
+const cli = cac();
+
+cli.option('--type <type>', 'Choose a project type', {
+  default: 'node',
+});
+
+const parsed = cli.parse();
+
+console.log(JSON.stringify(parsed, null, 2));
+
 console.log('⚡ Log ~ TradeZap');
