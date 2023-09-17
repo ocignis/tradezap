3035e68427aad6d7b47a8ec7b8c879bf9da60198
mkosir <kosir.mar@gmail.com>
2023-09-17 07:59:39 +0200

    fix(deps): update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { cac } from 'cac';
+// import { cac } from 'cac';
 
 console.log('##### ⚡ TradeZap #####');
 
-const cli = cac('tradezap');
+// const cli = cac('tradezap');
 
-cli.option('-c, --config <filename>', 'Custom config file', {
-  default: 'tradezap.config.ts',
-});
+// cli.option('-c, --config <filename>', 'Custom config file', {
+//   default: 'tradezap.config.ts',
+// });
 
-cli.option('-o, --out <dir>', 'Output directory', {
-  default: 'tradezap-data',
-});
+// cli.option('-o, --out <dir>', 'Output directory', {
+//   default: 'tradezap-data',
+// });
 
-cli.help();
-cli.version('0.0.0');
+// cli.help();
+// cli.version('0.0.0');
 
-cli.parse();
+// cli.parse();
 
 // const parsed = cli.parse();
 // console.log(JSON.stringify(parsed, null, 2));
