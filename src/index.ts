eb9f44f16e8169b093a4b093cd9a483f2d934daf
mkosir <kosir.mar@gmail.com>
2023-09-17 22:05:33 +0200

    fix(deps): update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 // import { cac } from 'cac';
 
-// import { TEST_VAR } from 'tradezap';
-
-// import { run } from 'demozap';
-// run();
-
 console.log('##### ⚡ TradeZap #####');
-
-export const TEST_VAR = 'test';
-
-console.log('🔎 Log ~ TEST_VAR:', TEST_VAR);
 
 // const cli = cac('tradezap');
 
@@ -28,3 +19,6 @@ console.log('🔎 Log ~ TEST_VAR:', TEST_VAR);
 
 // const parsed = cli.parse();
 // console.log(JSON.stringify(parsed, null, 2));
+
+export const TEST_VAR = 'test';
+console.log('🔎 Log ~ TEST_VAR:', TEST_VAR);
