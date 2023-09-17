ae0678f6567bf751fa25bab5499e8476d7f96119
mkosir <kosir.mar@gmail.com>
2023-09-17 22:37:58 +0200

    fix(deps): update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,24 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-empty-function */
-// import { cac } from 'cac';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-
-yargs(hideBin(process.argv))
-  .command(
-    'curl <url>',
-    'fetch the contents of the URL',
-    () => {},
-    (argv) => {
-      console.info(argv);
-    },
-  )
-  .demandCommand(1)
-  .parse();
+import { cac } from 'cac';
 
 console.log('##### ⚡ TradeZap #####');
 
-// const cli = cac('tradezap');
+const cli = cac('tradezap');
 
-// cli.option('-c, --config <filename>', 'Custom config file', {
-//   default: 'tradezap.config.ts',
-// });
+cli.option('-c, --config <filename>', 'Custom config file', {
+  default: 'tradezap.config.ts',
+});
 
-// cli.option('-o, --out <dir>', 'Output directory', {
-//   default: 'tradezap-data',
-// });
+cli.option('-o, --out <dir>', 'Output directory', {
+  default: 'tradezap-data',
+});
 
-// cli.help();
-// cli.version('0.0.0');
+cli.help();
+cli.version('0.0.0');
 
-// cli.parse();
+cli.parse();
 
-// const parsed = cli.parse();
-// console.log(JSON.stringify(parsed, null, 2));
+const parsed = cli.parse();
+console.log(JSON.stringify(parsed, null, 2));
 
 export const TEST_VAR = 'test';
 console.log('🔎 Log ~ TEST_VAR:', TEST_VAR);
