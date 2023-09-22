5d4ae44ab321c5f73894f5e8fb7cfb9ecb704883
mkosir <kosir.mar@gmail.com>
2023-09-22 22:22:32 +0200

    fix(deps): update dependencies

--- example/binance/tradezap.config.ts
+++ example/binance/tradezap.config.ts
@@ -8,10 +8,10 @@ const DATASETS_BINANCE: DatasetsBinance = [
         year: [2023],
         months: [5],
       },
-      {
-        year: [2022],
-        months: [10, 11, 12],
-      },
+      // {
+      //   year: [2022],
+      //   months: [10, 11, 12],
+      // },
       {
         year: [2021],
         months: [],
