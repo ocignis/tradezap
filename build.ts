9f21d215a9cca558e4791ff3f2be52f28cb1ee05
mkosir <kosir.mar@gmail.com>
2023-10-01 22:15:22 +0200

    fix(deps): update dependencies

--- build.ts
+++ build.ts
@@ -1,7 +1,7 @@
 import dts from 'bun-plugin-dts';
 
 await Bun.build({
-  entrypoints: ['./src/core/binance/index.ts', './src/core/binance/types.ts'],
+  entrypoints: ['./src/core/binance/index.ts', './src/core/binance/types/index.ts'],
   outdir: './dist',
   target: 'bun',
   format: 'esm',
