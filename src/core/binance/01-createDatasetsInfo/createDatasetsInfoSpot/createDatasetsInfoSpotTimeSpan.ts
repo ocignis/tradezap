dc440d5b960bbbb3e26d92c79854c084a4a58ffd
mkosir <kosir.mar@gmail.com>
2023-10-09 16:14:59 +0200

    fix(deps): update dependencies

--- src/core/binance/01-createDatasetsInfo/createDatasetsInfoSpot/createDatasetsInfoSpotTimeSpan.ts
+++ src/core/binance/01-createDatasetsInfo/createDatasetsInfoSpot/createDatasetsInfoSpotTimeSpan.ts
@@ -1,10 +1,10 @@
-import { TimeSpan } from 'core/binance/types/common';
+import { TimeSpan, Day, Month, Year } from 'core/binance/types/common';
 import { SpotAssetType } from 'core/binance/types/spot';
 
 import { DatasetInfo, DatasetsInfo } from '..';
 import { BASE_URL } from '../../consts';
 
-type createDatasetsInfoSpotTimeSpanParams = {
+type CreateDatasetsInfoSpotTimeSpanParams = {
   timeSpan: TimeSpan;
   pathOutputDirectory: string;
   tradingPair: string;
@@ -18,18 +18,12 @@ export const createDatasetsInfoSpotTimeSpan = ({
   tradingPair,
   assetType,
   asset,
-}: createDatasetsInfoSpotTimeSpanParams): DatasetsInfo => {
+}: CreateDatasetsInfoSpotTimeSpanParams): DatasetsInfo => {
   const { period } = timeSpan;
 
   const datasetsInfo = timeSpan.years.flatMap((year) =>
     timeSpan.months.map((month) => {
-      const monthFormatted = String(month).padStart(2, '0');
-
-      const day = period === 'daily' ? timeSpan.days.at(0) : null;
-      const dayFormatted = String(day).padStart(2, '0');
-      let datasetFilename = `${tradingPair}-${assetType}-${year}-${monthFormatted}`;
-      datasetFilename += day ? `-${dayFormatted}` : '';
-      datasetFilename += '.zip';
+      const datasetFilename = createDatasetFilename({ tradingPair, assetType, period, year, month, day: 1, asset });
 
       const datasetUrl = `${BASE_URL}/${asset}/${period}/${assetType}/${tradingPair}/${datasetFilename}`;
 
@@ -44,4 +38,38 @@ export const createDatasetsInfoSpotTimeSpan = ({
   );
 
   return datasetsInfo;
+};
+
+type CreateDatasetFilenameParams = {
+  tradingPair: string;
+  assetType: SpotAssetType;
+  period: TimeSpan['period'];
+  year: Year;
+  month: Month;
+  day: Day;
+  asset: 'spot';
+};
+
+const createDatasetFilename = ({
+  tradingPair,
+  assetType,
+  period,
+  year,
+  month,
+  day,
+}: CreateDatasetFilenameParams): string => {
+  const monthFormatted = String(month).padStart(2, '0');
+
+  const datasetFilenameBase = `${tradingPair}-${assetType}-${year}-${monthFormatted}`;
+
+  if (period === 'monthly') {
+    const datasetFilenameMonthly = `${datasetFilenameBase}.zip`;
+    return datasetFilenameMonthly;
+  }
+
+  const dayFormatted = String(day).padStart(2, '0');
+
+  const datasetFilenameDaily = `${datasetFilenameBase}-${dayFormatted}.zip`;
+
+  return datasetFilenameDaily;
 };
--- src/core/binance/types/common.ts
+++ src/core/binance/types/common.ts
@@ -22,12 +22,12 @@ export type TimeSpans = ReadonlyArray<TimeSpan>;
 export type TradingPair = `${string}-${string}`;
 
 export const YEARS = [2017, 2018, 2019, 2020, 2021, 2022, 2023] as const;
-type Year = (typeof YEARS)[number];
+export type Year = (typeof YEARS)[number];
 
 export const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const;
-type Month = (typeof MONTHS)[number];
+export type Month = (typeof MONTHS)[number];
 
 export const DAYS = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
 ] as const;
-type Day = (typeof DAYS)[number];
+export type Day = (typeof DAYS)[number];
