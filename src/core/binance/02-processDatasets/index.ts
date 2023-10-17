208597c81b3521905e1b330862b31bda4112f386
mkosir <kosir.mar@gmail.com>
2023-10-17 08:47:54 +0200

    fix(deps): update dependencies

--- src/core/binance/02-processDatasets/index.ts
+++ src/core/binance/02-processDatasets/index.ts
@@ -69,6 +69,7 @@ export const processDatasets = async ({
         // Opened issue - https://github.com/oven-sh/bun/issues/5970
         // await write(targetPath, file);
         const fileBlob = await response.blob();
+        //@ts-expect-error - Library type is wrong
         await write(targetPath, fileBlob);
       }
 
